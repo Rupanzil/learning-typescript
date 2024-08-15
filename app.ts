9ba6f418a31c931378ccdc438f652dfbdd83bd40
Rupanzil Prince <rupanzilmamunprince@gmail.com>
2024-08-16 00:48:43 +0530

    working example done

---
+++ app.ts
@@ -0,0 +1,10 @@
+// type annotations
+//variables
+let personName: string = 'John'
+personName = '123456'
+
+let age: number = 30
+
+// array
+let names: string[] = ['Alice', 'Bob']
+let ages: number[] = [25, 30]
---
+++ dist/app.js
@@ -0,0 +1,9 @@
+'use strict'
+// type annotations
+//variables
+let personName = 'John'
+personName = '123456'
+let age = 30
+// array
+let names = ['Alice', 'Bob']
+let ages = [25, 30]
